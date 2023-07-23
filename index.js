539f22f8f0bf8c1936b41f15020f5590945a68f2
lucaxartx <ujude1000@gmail>
2023-07-23 08:20:41 -0700

    fix: import neccessary route

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ const app = express();
 
 const db = require('./src/db/db');
 
+const accountRouter = require('./src/routes/account_route');
 
+const notFoundMiddleware = require('./src/middlewares/not-found');
+const errorHandlerMiddleware = require('./src/middlewares/error-handler');
+
+
+app.use(express.json());
+
+
+app.use('/api/v1/account', accountRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 const port = 3000;
